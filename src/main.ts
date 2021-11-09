169efcd90e5e7e5f061cf500710725a90d82fb8b
Hugo Bernier <hugoabernier@live.ca>
2021-11-08 23:37:25 -0500

    Updated release

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,40 @@
 import * as core from '@actions/core';
-import {warning as coreWarning, getInput as coreGetInput} from '@actions/core'
 import {context} from '@actions/github';
 
 async function run(): Promise<void> {
     try {
+        
+        // const issue: string = core.getInput('issue')
+        // core.debug(`Got issue: ${issue}  ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-        if (typeof context.issue.number !== 'undefined') {
-            core.debug(`Got issue from context: ${JSON.stringify(context.issue)}  ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-        }
-
-        // const body  = coreGetInput('body');
+        // const gitHubIssue  = JSON.parse(issue);
         // core.debug(`Got issue body: ${gitHubIssue.body} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+        const issueBody: string = core.getInput('issueBody');
+        const separator: string = core.getInput('separator');
+        const sectionName: string = core.getInput('sectionName');
+        core.debug(`Got issue body: "${issueBody}"`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+        core.debug(`Got separator: "${separator}"`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+        core.debug(`Got sectionName: "${sectionName}"`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-        // const bodySections: Array<string> = gitHubIssue.body.split("### ");
-        // core.debug(`Got issue body lines: ${JSON.stringify(bodySections)} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+        const bodySections: Array<string> = issueBody.split(separator);
+        core.debug(`Got issue body lines: ${JSON.stringify(bodySections)} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-        // const bodyArray: { [key: string]: string } = {};
-        // bodySections.forEach(section => {
-        //     // Split the section into header and content            
-        //     const sectionParts: Array<string> = section.split("\n\n", 2);
-        //     if (sectionParts.length === 2 && sectionParts[0] !== "") {
-        //         bodyArray[sectionParts[0]] = sectionParts[1];
-        //         core.debug(`Section Part: '${sectionParts[0]}': '${sectionParts[1]}'`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-        //     }
-        // });
+        const bodyArray: { [key: string]: string } = {};
+        bodySections.forEach(section => {
+            // Split the section into header and content            
+            const sectionParts: Array<string> = section.split("\n\n", 2);
+            if (sectionParts.length === 2 && sectionParts[0] !== "") {
+                bodyArray[sectionParts[0]] = sectionParts[1];
+                core.debug(`Section Part: '${sectionParts[0]}': '${sectionParts[1]}'`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+            }
+        });
 
-        // core.setOutput('authors', bodyArray['Author(s)']);
-        // if (bodyArray['Author(s)'].indexOf('@') > -1) {
-        //     core.setOutput('valid-authors',bodyArray['Author(s)']);
-        // } else {
-        //     core.setOutput('valid-authors',"");
-        // }
-        core.setOutput('valid-authors',"");
+        core.setOutput('authors', bodyArray['Author(s)']);
+        if (bodyArray['Author(s)'].indexOf('@') > -1) {
+            core.setOutput('valid-authors',bodyArray['Author(s)']);
+        } else {
+            core.setOutput('valid-authors',"");
+        }
         
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
